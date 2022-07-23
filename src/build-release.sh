83134db5cb959d4cb77720a82c6eed08537ee6d8
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2022-07-23 17:32:45 +0200

    Updated build scripts

--- src/build-release.sh
+++ src/build-release.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+#: Title       : build-release.sh
+#: Date        : 2022-07-23
+#: Author      : Kjetil Kristoffer Solberg <post@ikjetil.no>
+#: Version     : 1.0
+#: Description : Builds ccat.
 echo "Building its-strip ..."
 echo "> using release build < "
 
 g++ its-strip.cpp -o its-strip
 if [[ $? -eq 0 ]]
 then
-    echo "> its-strip build Ok"
+    echo "> its-strip build ok"
 else
-    echo "> its-strip build Error"
+    echo "> its-strip build error"
 fi
 
-echo "> Build Process Complete <"
+echo "> build process complete <"
