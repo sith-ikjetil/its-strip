68da33824823120988fcbce5b18b6546ebb37f02
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2023-09-25 12:38:43 +0200

    Improved man pages install

--- man/install.sh
+++ man/install.sh
@@ -1,9 +1,38 @@
+function InstallUbuntu()
+{
+    if [ ! -d "/usr/local/man/man1" ]
+    then
+        sudo mkdir /usr/local/man/man1
+    fi
+
+    sudo cp its-strip.1 /usr/local/man/man1
+    sudo chmod go+r /usr/local/man/man1/its-strip.1
+    printf "Install Ubuntu Complete\n"
+}
+
+function InstallFedora()
+{
+    # fedora
+    if [ ! -d "/usr/local/share/man/man1" ]
+    then
+        sudo mkdir "/usr/local/share/man/man1"
+    fi        
+    sudo cp its-strip.1 /usr/local/share/man/man1
+    sudo chmod go+r /usr/local/share/man/man1/its-strip.1
+    printf "Install Fedora Complete\n"
+}
+
+
 printf "Installing man page\n"
+
 ./render.sh
-if [ ! -d "/usr/local/man/man1" ]
+
+if [ -d "/usr/local/man" ]
 then
-    sudo mkdir /usr/local/man/man1
+    InstallUbuntu
 fi
-sudo cp its-strip.1 /usr/local/man/man1
-sudo chmod go+r /usr/local/man/man1/its-strip.1
-printf "...install complete\n"
+
+if [ -d "/usr/local/share/man" ]
+then
+    InstallFedora
+fi
